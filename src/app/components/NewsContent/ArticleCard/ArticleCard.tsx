338247e144be3b53f6b3da4721035e2015981277
Pekay <github@maxlingenfelter.com>
2023-07-29 12:31:54 -0400

    feat: :sparkles: Updates

--- src/app/components/NewsContent/ArticleCard/ArticleCard.tsx
+++ src/app/components/NewsContent/ArticleCard/ArticleCard.tsx
@@ -6,27 +6,46 @@ import Image from 'next/image'
 interface ArticleCardProps {
     title: string;
     content: string;
-    //Date format in 2023-07-26T17:19:52+00:00
     date: string;
+    image: string | null;
+    url: string;
+    source: string
+    topArticle: boolean;
 }
 
-export default function ArticleCard({ title, content, date }: ArticleCardProps) {
+export default function ArticleCard({ title, content, date, image, url, source, topArticle }: ArticleCardProps) {
 
     // Render date string in format: "July 27th, 2023" format from 2023-07-26T17:19:52+00:00
     const dateString = new Date(date).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })
 
+    //Remove tld from source
+    const nonUrlSource = source.replace('.com', '').replace('.co.uk', '')
     return (
-        <div className={styles.card}>
-            <div className={styles.newsHeader}>
-                {title}
-            </div>
-            <div className={styles.date}>
-                {dateString}
-            </div>
-            <div className={styles.content}>
-                <p>{content}</p>
-            </div>
-            <Image />
-        </div >
+        <a className={styles.cardWrapper} href={url} target='_blank'>
+
+            {/* If top article is true show top article card */}
+            {topArticle && (
+                <div className={styles.topArticleCard}>
+                    <p className={styles.topArticleText}>TOP OF THE DAY</p>
+                </div>
+            )}
+
+            <div className={`${styles.card} ${topArticle ? styles.topArticle : ''}`}>
+
+                <div className={styles.newsHeader}>
+                    {title}
+                </div>
+                <div className={styles.date}>
+                    {dateString} • {nonUrlSource}
+                </div>
+                <div className={styles.content}>
+                    <p>{content}</p>
+                </div>
+                {/* If image defined */}
+                {image && (
+                    <img src={image} alt={title} className={styles.image} />
+                )}
+            </div >
+        </a >
     )
 }
--- src/app/components/NewsContent/NewsContent.tsx
+++ src/app/components/NewsContent/NewsContent.tsx
@@ -5,17 +5,106 @@ import styles from './NewsContent.module.scss'
 
 export default function NewsContent() {
 
+    const sampleData = [
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+    ]
+
+
+
+
     return (
         <section className={styles.wrapper}>
             <div className={styles.articles}>
-                <ArticleCard
-                    title='Nadal withdraws from U.S. Open'
-                    content='Rafael Nadal is officially OUT of the U.S. Open the tennis legend said Tuesday...'
-                    // Date in unix time
-                    date={"2023-07-26T17:19:52+00:00"}
-                />
+                {sampleData.map((article, index) => {
+                    // Limit title to 5 words and add ellipsis
+                    const title = article.title.split(' ').slice(0, 7).join(' ') + '...'
+                    //Limit content to 20 words and add ellipsis
+                    const content = article.description.split(' ').slice(0, 16).join(' ') + '...'
+                    return (
+                        <ArticleCard
+                            key={index}
+                            title={title}
+                            content={content}
+                            date={article.published_at}
+                            image={article.image}
+                            source={article.source}
+                            url={article.url}
+                            topArticle={index === 0}
+                        />
+                    )
+                })}
             </div>
-
-        </section >
+        </section>
     )
 }
