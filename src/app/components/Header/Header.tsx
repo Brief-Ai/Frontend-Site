9fa609d8e18dd66de6dd3d56b746075764c0f47d
Pekay <github@maxlingenfelter.com>
2023-07-30 01:50:18 -0400

    Update Header.tsx

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -4,6 +4,80 @@ import styles from './Header.module.scss'
 import Image from 'next/image'
 
 export default function Header() {
+    const sampleData = [
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+        {
+            "author": "TMZ Staff",
+            "title": "Rafael Nadal Pulls Out Of U.S. Open Over COVID-19 Concerns",
+            "description": "Rafael Nadal is officially OUT of the U.S. Open ... the tennis legend said Tuesday it's just too damn unsafe for him to travel to America during the COVID-19 pandemic. \"The situation is very complicated worldwide,\" Nadal wrote in a statement. \"The…",
+            "url": "https://www.tmz.com/2020/08/04/rafael-nadal-us-open-tennis-covid-19-concerns/",
+            "source": "TMZ.com",
+            "image": "https://imagez.tmz.com/image/fa/4by3/2020/08/04/fad55ee236fc4033ba324e941bb8c8b7_md.jpg",
+            "category": "general",
+            "language": "en",
+            "country": "us",
+            "published_at": "2020-08-05T05:47:24+00:00"
+        },
+    ]
 
     return (
         <section className={styles.wrapper}>
@@ -28,6 +102,21 @@ export default function Header() {
                     height={23.11}
                 />
                 <input type="text" className={styles.searchBarInput} placeholder="Search relevant news..." />
+                <div className={styles.searchResults}>
+                    {/* List  */}
+                    <ul className={styles.searchResultsList}>
+                        {sampleData.map((result, index) => (
+                            <li className={styles.searchResultItem} key={index}>
+                                <div className={styles.searchResultItemTitle}>
+                                    {result.title}
+                                </div>
+                                <div className={styles.searchResultItemSource}>
+                                    {result.source}
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
             <div className={styles.actionButtonWrapper}>
                 <div className={[styles.btn, styles.signUpBtn].join(" ")} >
